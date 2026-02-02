4625ffd102d51e491e136d601d926dfa721a21e8
liviaUeno <livs.ueno@gmail.com>
2026-02-02 13:12:38 -0300

    feat: adapt auth module for paywall

--- modules/rest_auth_provider/rest_auth_provider.py
+++ modules/rest_auth_provider/rest_auth_provider.py
@@ -1,202 +1,141 @@
-# -*- coding: utf-8 -*-
-#
-# REST endpoint Authentication module for Matrix synapse
-# Copyright (C) 2017 Kamax Sarl
-#
-# https://www.kamax.io/
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-
 import logging
+import re
 import requests
-import json
-import time
-from synapse.types import UserID
+import hashlib
+import unicodedata
 
+from synapse.api.errors import AuthError
+from synapse.types import UserID
 
 logger = logging.getLogger(__name__)
 
-logger.warning("USING REST AUTH PROVIDER FROM: %s", __file__)
 
-class RestAuthProvider(object):
+class RestAuthProvider:
 
     def __init__(self, config, account_handler):
         self.account_handler = account_handler
 
-        if not config.endpoint:
-            raise RuntimeError('Missing endpoint config')
+        self.api_base = config["api_base"]
+        self.check_endpoint = "/public/authenticate"
 
-        self.endpoint = config.endpoint
-        self.regLower = config.regLower
-        self.config = config
+        self.homeserver = config["homeserver"]
+        self.timeout = config.get("timeout", 5)
 
-        logger.info('Endpoint: %s', self.endpoint)
-        logger.info('Enforce lowercase username during registration: %s', self.regLower)
-
-    async def check_password(self, user_id, password):
-        logger.info("Got password check for " + user_id)
-        data = {'user': {'id': user_id, 'password': password}}
-        r = requests.post(self.endpoint + '/_matrix-internal/identity/v1/check_credentials', json=data)
-        r.raise_for_status()
-        r = r.json()
-        if not r["auth"]:
-            reason = "Invalid JSON data returned from REST endpoint"
-            logger.warning(reason)
-            raise RuntimeError(reason)
-
-        auth = r["auth"]
-        if not auth["success"]:
-            logger.info("User not authenticated")
-            return False
-
-        localpart = user_id.split(":", 1)[0][1:]
-        logger.info("User %s authenticated", user_id)
-
-        registration = False
-
-        if not (await self.account_handler.check_user_exists(user_id)):
-            logger.info("User %s does not exist yet, creating...", user_id)
-
-            if localpart != localpart.lower() and self.regLower:
-                logger.info('User %s cannot be created due to lowercase policy', localpart)
-                return False
-
-            await self.account_handler.register(localpart=localpart)
-            registration = True
-            logger.info("Registration based on REST data was successful for %s", user_id)
-        else:
-            logger.info("User %s already exists, registration skipped", user_id)
-
-        user_obj = UserID.from_string(user_id)
-
-        if auth.get("profile"):
-            logger.info("Handling profile data")
-            profile = auth["profile"]
-
-            store = self.account_handler._hs.get_datastores().main
-
-            if "display_name" in profile and (
-                (registration and self.config.setNameOnRegister)
-                or (self.config.setNameOnLogin)
-            ):
-                display_name = profile["display_name"]
-                logger.info("Setting display name to '%s'", display_name)
-                await store.set_profile_displayname(user_obj, display_name)
-
-            if self.config.updateThreepid and "three_pids" in profile:
-                logger.info("Handling 3PIDs")
-
-                external_3pids = []
-
-                for threepid in profile["three_pids"]:
-                    medium = threepid["medium"].lower()
-                    address = threepid["address"].lower()
-                    external_3pids.append({"medium": medium, "address": address})
-
-                    validated_at = time_msec()
-
-                    if not (await store.get_user_id_by_threepid(medium, address)):
-                        logger.info("3PID is not present, adding")
-                        await store.user_add_threepid(
-                            user_id=user_obj.to_string(),
-                            medium=medium,
-                            address=address,
-                            validated_at=validated_at,
-                            added_at=validated_at
-                        )
-
-        else:
-            logger.info("No profile data")
-
-        return True
+        logger.info("RestAuthProvider carregado")
 
     @staticmethod
     def parse_config(config):
-        # verify config sanity
-        _require_keys(config, ["endpoint"])
+        for key in ("api_base", "homeserver"):
+            if key not in config:
+                raise Exception(f"{key} é obrigatório")
+        return config
 
-        class _RestConfig(object):
-            endpoint = ''
-            regLower = True
-            setNameOnRegister = True
-            setNameOnLogin = False
-            updateThreepid = True
-            replaceThreepid = False
+    @staticmethod
+    def get_supported_login_types():
+        return {
+            "m.login.password": ("password",),
+        }
 
-        rest_config = _RestConfig()
-        rest_config.endpoint = config["endpoint"]
+    async def check_auth(self, username, login_type, login_dict):
+        password = login_dict.get("password")
+        if not password:
+            raise AuthError(403, "Senha obrigatória")
 
+        email = self._extract_email(username)
+        if not email:
+            raise AuthError(400, "Email obrigatório")
+
+        logger.info("Login request email=%s", email)
+
+        data = self._check_paywall(email, password)
+
+        localpart = self._build_localpart_from_user(data)
+        mxid = f"@{localpart}:{self.homeserver}"
+
+        logger.info("MXID resolvido: %s", mxid)
+
+        await self._ensure_user_exists(mxid)
+
+        return (mxid, None)
+
+    def _check_paywall(self, email, password):
         try:
-            rest_config.regLower = config['policy']['registration']['username']['enforceLowercase']
-        except TypeError:
-            # we don't care
-            pass
-        except KeyError:
-            # we don't care
-            pass
-
-        try:
-            rest_config.setNameOnRegister = config['policy']['registration']['profile']['name']
-        except TypeError:
-            # we don't care
-            pass
-        except KeyError:
-            # we don't care
-            pass
-
-        try:
-            rest_config.setNameOnLogin = config['policy']['login']['profile']['name']
-        except TypeError:
-            # we don't care
-            pass
-        except KeyError:
-            # we don't care
-            pass
-
-        try:
-            rest_config.updateThreepid = config['policy']['all']['threepid']['update']
-        except TypeError:
-            # we don't care
-            pass
-        except KeyError:
-            # we don't care
-            pass
-
-        try:
-            rest_config.replaceThreepid = config['policy']['all']['threepid']['replace']
-        except TypeError:
-            # we don't care
-            pass
-        except KeyError:
-            # we don't care
-            pass
-
-        return rest_config
-
-
-def _require_keys(config, required):
-    missing = [key for key in required if key not in config]
-    if missing:
-        raise Exception(
-            "REST Auth enabled but missing required config values: {}".format(
-                ", ".join(missing)
+            resp = requests.post(
+                f"{self.api_base}{self.check_endpoint}",
+                json={
+                    "email": email,
+                    "password": RestAuthProvider.hash_password(password),
+                },
+                timeout=self.timeout,
             )
+        except Exception as e:
+            logger.error("Erro chamando paywall: %s", e)
+            raise AuthError(500, "Erro no serviço de autenticação")
+
+        if resp.status_code != 200:
+            raise AuthError(403, "Credenciais inválidas")
+
+        data = resp.json()
+
+        if not isinstance(data, dict) or "email" not in data:
+            raise AuthError(403, "Resposta inválida do paywall")
+
+        return data
+
+ 
+    async def _ensure_user_exists(self, mxid: str):
+        if await self.account_handler.check_user_exists(mxid):
+            logger.info("Usuário %s já existe", mxid)
+            return
+
+        logger.info("Criando usuário %s", mxid)
+
+        localpart = mxid.split(":", 1)[0][1:]
+
+        await self.account_handler.register(
+            localpart=localpart,
+            displayname=None,
         )
 
 
-def time_msec():
-    """Get the current timestamp in milliseconds
-    """
-    return int(time.time() * 1000)
+    def _extract_email(self, username):
+        
+        if isinstance(username, str) and "@" in username:
+            return username.strip().lower()
+        return None
+
+    def _build_localpart_from_user(self, data: dict) -> str:
+        user_id = data.get("id")
+        email = data.get("email")
+
+        if not user_id:
+            raise AuthError(403, "ID do usuário ausente no paywall")
+
+        info = data.get("info") or {}
+        first = info.get("first-name")
+        last = info.get("last-name")
+
+        if first and last:
+            base = f"{first}_{last}"
+
+        elif email:
+            base = email.split("@", 1)[0]
+
+        else:
+            base = "user"
+
+        base = self._normalize_localpart(base)
+
+        # suffix = hashlib.sha256(user_id.encode()).hexdigest()[:6]
+
+        return f"{base}" # _{suffix}
+
+    def _normalize_localpart(self, value: str) -> str:
+        value = unicodedata.normalize("NFKD", value)
+        value = value.encode("ascii", "ignore").decode("ascii")
+        value = value.lower()
+        return re.sub(r"[^a-z0-9._=-]", "_", value)
+
+    @staticmethod
+    def hash_password(raw: str) -> str:
+        return hashlib.sha512(raw.encode("utf-8")).hexdigest()
